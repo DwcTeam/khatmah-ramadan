2354ee7c787f86083cd8dc3a88b55363e769494d
HazemMeqdad <66125469+HazemMeqdad@users.noreply.github.com>
2022-04-02 12:39:16 +0300

    rename a plugin

--- bot/exts/interaction.py
+++ bot/exts/interaction.py
@@ -1,18 +1,17 @@
 import hikari
 import lightbulb
-from lightbulb.ext import tasks
 
-task = lightbulb.Plugin(__name__)
+interaction = lightbulb.Plugin(__name__)
 
 
-@task.listener(hikari.InteractionCreateEvent)
+@interaction.listener(hikari.InteractionCreateEvent)
 async def interaction_create_event(event: hikari.InteractionCreateEvent) -> None:
     if event.interaction.custom_id == "add_role" and event.interaction.type == hikari.InteractionType.BUTTON:
         guild_id = event.interaction.member.guild_id
-        data = task.bot.db.find_one({"_id": guild_id})
+        data = interaction.bot.db.find_one({"_id": guild_id})
         if not data or data["channel"] != event.interaction.channel_id:
             return
-        if not data["role"] or not task.bot.cache.get_role(data["role"]):
+        if not data["role"] or not interaction.bot.cache.get_role(data["role"]):
             await event.app.rest.create_interaction_response(
                 interaction=event.interaction,
                 token=event.interaction.token,
@@ -21,8 +20,8 @@ async def interaction_create_event(event: hikari.InteractionCreateEvent) -> None
                 flags=hikari.MessageFlag.EPHEMERAL
             )
             return
-        role = task.bot.cache.get_role(data["role"])
-        user = task.bot.cache.get_member(guild_id, event.interaction.member.user)
+        role = interaction.bot.cache.get_role(data["role"])
+        user = interaction.bot.cache.get_member(guild_id, event.interaction.member.user)
         if not user:
             user = await event.app.rest.fetch_member(guild_id, event.interaction.member.user)
         role_ids = user.role_ids
@@ -35,7 +34,7 @@ async def interaction_create_event(event: hikari.InteractionCreateEvent) -> None
                 content="تم الغاء أشتراك بالرتبه بنجاح",
                 flags=hikari.MessageFlag.EPHEMERAL
             )
-            task.bot.db.update_one({"_id": guild_id}, {"$set": {"count": data["count"] - 1}})
+            interaction.bot.db.update_one({"_id": guild_id}, {"$set": {"count": data["count"] - 1}})
         else:
             await event.app.rest.add_role_to_member(guild_id, user, role)
             await event.app.rest.create_interaction_response(
@@ -45,8 +44,8 @@ async def interaction_create_event(event: hikari.InteractionCreateEvent) -> None
                 content="تمت أضافتك للخمته بنجاح",
                 flags=hikari.MessageFlag.EPHEMERAL
             )
-            task.bot.db.update_one({"_id": guild_id}, {"$set": {"count": data["count"] + 1}})
+            interaction.bot.db.update_one({"_id": guild_id}, {"$set": {"count": data["count"] + 1}})
 
 
 def load(bot: lightbulb.BotApp) -> None:
-    bot.add_plugin(task)
+    bot.add_plugin(interaction)
